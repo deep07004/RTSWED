9ef6e71dbda9667544e3e84d101b0dec5087465b
Dipankar Saikia <deepngri@gmail.com>
2022-10-19 15:06:07 +0530

    triads formed

--- maketriad.py
+++ maketriad.py
@@ -13,11 +13,46 @@
 #   ntriad : no of triads
 from scipy.spatial import Delaunay
 import numpy as np
+import pandas as pd
+from geographiclib.geodesic import Geodesic
 
-def maketriads(stations, minlen=10, maxlen=600, minang=30, maxang=120):
-    nsta = len(stations)
-    pp = np.zeros((nsta,2))
-    for i in range(nsta):
-        pp[i,0] = stations[i,1]
-        pp[i,1] = stations[i,2]
-    return triads, ntriad
+gl = Geodesic.WGS84
+
+def maketriad(inv, minlen=10, maxlen=600, minang=30, maxang=120):
+    sta_list = []
+    for nt in inv.networks:
+        for sta in nt.stations:
+            sta_list.append([nt.code+'.'+sta.code,sta.latitude,sta.longitude,sta.elevation])
+
+    stations = pd.DataFrame(sta_list)
+    stations.set_index(0,inplace=True)
+    pp = np.array(stations[[2,1]])
+    tri = Delaunay(pp)
+    ss=(np.zeros(np.shape(tri.simplices)[0])==0)
+    # Check the side length
+    # Check for interior angle
+    for l, xx in enumerate(tri.simplices):
+        dist = np.zeros(3)
+        ang = np.zeros(3)
+        dist[0] = gl.Inverse(pp[xx[0],1],pp[xx[0],0],pp[xx[1],1],pp[xx[1],0])['s12']/1000.0
+        dist[1] = gl.Inverse(pp[xx[0],1],pp[xx[0],0],pp[xx[2],1],pp[xx[2],0])['s12']/1000.0
+        dist[2] = gl.Inverse(pp[xx[1],1],pp[xx[1],0],pp[xx[2],1],pp[xx[2],0])['s12']/1000.0
+        dist.sort()
+        ang[0] = np.arccos((dist[1]**2 + dist[2] **2 - dist[0] **2) / (2 * dist[1] * dist[2]))
+        ang[1] = np.arccos((dist[0]**2 + dist[2] **2 - dist[1] **2) / (2 * dist[0] * dist[2]))
+        ang[2] = np.arccos((dist[0]**2 + dist[1] **2 - dist[2] **2) / (2 * dist[0] * dist[1]))
+        ang *= 180.0/np.pi
+        ang.sort()
+        if dist[0] < minlen or dist[2] > maxlen :
+            ss[l] = False
+        if ang[0] < minang or ang[2] > maxang:
+            ss[l] = False
+    traids_list = []
+    for t in tri.simplices[ss]:
+        _tmp =[]
+        for i in t:
+            _a  = stations[stations[1] == pp[i,1]]
+            _tmp.append(_a[_a[2] == pp[i,0]].index[0])
+        traids_list.append(_tmp)
+    triads = pd.DataFrame(traids_list)
+    return triads, stations
--- test.py
+++ test.py
@@ -1,52 +1,29 @@
 #!/usr/bin/env python
-import sys
-from scipy.spatial import Delaunay
-import numpy as np
-import matplotlib.pyplot as plt
-from geographiclib.geodesic import Geodesic
 from obspy import read_inventory
+import pandas as pd
+import pygmt
+import maketriad
 
-gl = Geodesic.WGS84
-
-sta_list=[]
-inv = read_inventory(sys.argv[1])
-for nt in inv.networks:
-    for sta in nt.stations:
-        sta_list.append([nt.code+'.'+sta.code,sta.latitude,sta.longitude,sta.elevation])
-n=len(sta_list)
-pp = np.zeros((n,2))
-for i in range(n):
-    ff = sta_list[i]
-    pp[i,0] = np.float64(ff[2])
-    pp[i,1] = np.float64(ff[1])
-tri = Delaunay(pp)
-ss=(np.zeros(np.shape(tri.simplices)[0])==0)
-### for l, xx in enumerate(tri.simplices):
-###     for i in range(3):
-###         j = (i+1)%3
-###         a = xx[i]
-###         b = xx[j]
-###     # Check the side length
-###         side_length = gl.Inverse(pp[a,1],pp[a,0],pp[b,1],pp[b,0])
-###         if side_length['s12']/1000 < 10 or side_length['s12']/1000 > 600:
-###             ss[l] = False
-###     # Check for interior angle
-for l, xx in enumerate(tri.simplices):
-    dist = np.zeros(3)
-    ang = np.zeros(3)
-    dist[0] = gl.Inverse(pp[xx[0],1],pp[xx[0],0],pp[xx[1],1],pp[xx[1],0])['s12']/1000.0
-    dist[1] = gl.Inverse(pp[xx[0],1],pp[xx[0],0],pp[xx[2],1],pp[xx[2],0])['s12']/1000.0
-    dist[2] = gl.Inverse(pp[xx[1],1],pp[xx[1],0],pp[xx[2],1],pp[xx[2],0])['s12']/1000.0
-    dist.sort()
-    ang[0] = np.arccos((dist[1]**2 + dist[2] **2 - dist[0] **2) / (2 * dist[1] * dist[2]))
-    ang[1] = np.arccos((dist[0]**2 + dist[2] **2 - dist[1] **2) / (2 * dist[0] * dist[2]))
-    ang[2] = np.arccos((dist[0]**2 + dist[1] **2 - dist[2] **2) / (2 * dist[0] * dist[1]))
-    ang *= 180.0/np.pi
-    ang.sort()
-    if dist[0] < 50 or dist[2] > 600 :
-        ss[l] = False
-    if ang[0] < 30 or ang[2] > 120:
-        ss[l] = False
-
-plt.triplot(pp[:,0],pp[:,1],tri.simplices[ss])
-plt.show()
+inv = read_inventory('Inventory.xml')
+triads, stations = maketriad.maketriad(inv, minlen=10, maxlen=1200, minang=10, maxang=120)
+reg = [ 105, 182, -48, -2 ]
+grid = pygmt.datasets.load_earth_relief(resolution="02m", region=reg)
+fig = pygmt.Figure()
+pygmt.makecpt(cmap="terra", series=[-8000, 8000])
+fig.basemap(region=reg, projection="M15c",frame=True)
+fig.grdimage(grid=grid,shading=True)
+for i in triads.index:
+    lat = []
+    lon = []
+    for a in triads.loc[i]:
+        _tmp = stations.loc[a]
+        lat.append(_tmp[1])
+        lon.append(_tmp[2])
+    lat.append(lat[0])
+    lon.append(lon[0])
+#    for j in range(3):
+#        fig.plot(x=lon[j:j+2], y=lat[j:j+2], pen='0.35p')
+fig.plot(stations[[2,1]],style="t0.25c", color="red")
+fig.savefig('triad.jpg')
+fig.show()
+print(triads)
