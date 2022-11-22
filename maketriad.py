8c0cb059c9f597c82e86e726cb9f6cbe1b74c09d
dipankar Saikia <deepngri@gmail.com>
2022-11-23 04:22:49 +0530

    test

--- maketriad.py
+++ maketriad.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
 
-# This program form triad subarrays from the input station list
-# using delaunay trianglation
-# Inputs:
-#   station : array with station information ['ID', lat, lon, elevation]
-#   minlen : minimum side length of a triad in km
-#   maxlen : maximum side length of a triad in km
-#   minang : minimum interior angle of a triad
-#   maxang : maximum interior angle of a triad
-# Outoput:
-#   traids :  array ['ID', lat, lon, elevation, triad index]
-#   ntriad : no of triads
 from scipy.spatial import Delaunay
 import numpy as np
 import pandas as pd
 from geographiclib.geodesic import Geodesic
 
-gl = Geodesic.WGS84
-
 def maketriad(inv, minlen=10, maxlen=600, minang=30, maxang=120):
+    """
+    This program form triad subarrays from the input station inventory (fdsnxml)
+    using delaunay trianglation
+    Inputs:
+      inv    : station inventory in fdsnxml format
+      minlen : minimum side length of a triad in km
+      maxlen : maximum side length of a triad in km
+      minang : minimum interior angle of a triad
+      maxang : maximum interior angle of a triad
+    Outoput:
+      traids : panda data frame of triads containing nt.code of
+               three stations in each row.
+      stations : panda data frame of stations indexed by nt.sta
+                column 1 is latitude, column 2 is longitude and
+                column 3 is elevation 
+    """
+    gl = Geodesic.WGS84
     sta_list = []
     for nt in inv.networks:
         for sta in nt.stations:
