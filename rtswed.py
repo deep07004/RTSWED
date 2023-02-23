bafbca8d1f3bb3717aec21e3cac517926a8d9672
dipankar Saikia <deepngri@gmail.com>
2023-02-23 17:34:43 +0530

    config

--- rtswed.py
+++ rtswed.py
@@ -21,23 +21,27 @@ args = parser.parse_args()
 # Read the configuration file and check that all informations are there 
 # to proceed further 
 cfg = read(args.config)
-inv = read_inventory('Inventory.xml')
+inv = read_inventory(os.path.join(cfg['Work_dir'],cfg['Inventory']))
 #inv =  read_inventory('../Turkey.xml')
-triads = maketriad.maketriad(inv, minlen=10, maxlen=1200, minang=10, maxang=120)
+triads = maketriad.maketriad(inv, minlen=cfg['Triad']['min_side_length'], \
+    maxlen=cfg['Triad']['max_side_length'], minang=cfg['Triad']['min_angle'],\
+    maxang=cfg['Triad']['max_angle'])
 
-t1 = UTCDateTime("2022-1-15T04:14:45")
+ot = UTCDateTime("2022-1-15T04:14:45")
 #t1 = UTCDateTime("2023-02-06T01:17:35") + 400
-t2= t1+3600
-
-triads.get_waveform(start=t1,end=t2,source = ['sds','/home/deep/CWP/Landslide/Tonga/buffer'])
-#triads.get_waveform(start=t1,end=t2,source = ['sds','/home/deep/CWP/Landslide/Turkey'])
-
-for td in triads:
-    td.correlate(shift=3000)
-    td.beamform()
-a = triads.select_active()
-for i, td in enumerate(triads):
-    if td.detection:
-        s = "%d %0.4f %0.4f %0.2f %0.2f %s %0.2f\n" %(i,td.clat, td.clon, td.azm, td.apvel, td.beam[0],td.beam[1])
-        print(s)
-triads.map_plot(center=[140,-35])
+fp = open("out.txt","w")
+for i in range(13):
+    t1= ot + 180*i
+    t2 = t1 + 600
+    triads.get_waveform(start=t1,end=t2,cfg=cfg)
+    for td in triads:
+        td.correlate(shift=3000)
+        td.beamform()
+    a = triads.select_active()
+    for i, td in enumerate(triads):
+        if td.detection:
+            s = "%s,%0.4f,%0.4f,%0.2f,%0.2f,%s,%0.2f\n" %(t1,td.clat, td.clon, td.azm, td.apvel, td.beam[0],td.beam[1])
+            print(s)
+            fp.write(s)
+fp.close()
+    #triads.map_plot(center=[140,-35])
--- triad.py
+++ triad.py
@@ -2,24 +2,33 @@ import numpy as np
 import copy
 from obspy import UTCDateTime, Stream
 from obspy.clients.filesystem.sds import Client as SDS
+from obspy.clients.fdsn.client import Client
 from geographiclib.geodesic import Geodesic
 from obspy.signal import cross_correlation
 from obspy.signal.filter import envelope
 
-def request_data(i, td, start, end, sps, sds_arc):
-    sds = SDS(sds_arc)
+def request_data(i, td, start, end, sps,fl,fh, method):
     st = Stream()
-    for sta in td.stations:
-        s = sta[0].split('.')
+    streams = [(sta[0].split('.')[0], sta[0].split('.')[1],'*',"?HZ",start, end) for sta in td.stations]
+
+    if method[0] == "SDS":
+        sds = SDS(method[1])
         try:
-            st += sds.get_waveforms(s[0],s[1],"*", "?HZ", start, end)[0]
+            st += sds.get_waveforms_bulk(streams)
         except:
-            break
+            pass
+    if method[0] == "FDSNWS":
+        fdsnws = Client(method[1])
+        try:
+            st += fdsnws.get_waveforms_bulk(streams)
+        except:
+            pass
+
     if st.count() > 0:
         st.merge(fill_value=0)
         st.detrend("demean")
         st.detrend("linear")
-        st.filter('bandpass', freqmin=1/250, freqmax=1/50)
+        st.filter('bandpass', freqmin=fl, freqmax=fh)
         st.resample(sps, window='cosine')
         sss = [tr.stats.starttime for tr in st]
         eee = [tr.stats.endtime for tr in st]
@@ -31,6 +40,16 @@ def request_data(i, td, start, end, sps, sds_arc):
             st.clear()
         else:
             st.trim(t1,t2)
+    
+    tr = Stream()
+    for sta in td.stations:
+        s = sta[0].split('.')[1]
+        try:
+            tr +=  st.select(station=s).sort(reverse=True)[0]
+        except:
+            print("No data for stations %s" %sta)
+            return (-1,-1)
+    st = tr.copy()
     if st.count() == 3:
         st.normalize()
         for tr in st:
@@ -109,12 +128,15 @@ class Triad(object):
     
     def correlate(self,shift=300):
         if self.have_data:
-            a = cross_correlation.correlate(self.data[0].data,self.data[1].data,shift=shift)
-            b = cross_correlation.correlate(self.data[0].data,self.data[2].data,shift=shift)
-            c = cross_correlation.correlate(self.data[1].data,self.data[2].data,shift=shift)
+            sps = self.data[0].stats.sampling_rate
+            shift_sps = int(shift * sps)
+            a = cross_correlation.correlate(self.data[0].data,self.data[1].data,shift=shift_sps)
+            b = cross_correlation.correlate(self.data[0].data,self.data[2].data,shift=shift_sps)
+            c = cross_correlation.correlate(self.data[1].data,self.data[2].data,shift=shift_sps)
             self.dt[0], self.cc[0] = cross_correlation.xcorr_max(a)
             self.dt[1], self.cc[1] = cross_correlation.xcorr_max(b)
             self.dt[2], self.cc[2] = cross_correlation.xcorr_max(c)
+            self.dt /= sps
             if np.amax(self.cc) >= 0.5 and abs(np.sum(self.dt)) <=50:
                 alpha = np.linalg.lstsq(self.Amat, self.dt,rcond=None)[0]
                 self.alpha = alpha
@@ -129,9 +151,10 @@ class Triad(object):
     def beamform(self):
         if self.detection:
             tr = self.data[0].copy()
-            t1 = np.ceil(self.CX[0]*self.alpha[0] + self.CY[0]*self.alpha[1])
-            t2 = np.ceil(self.CX[1]*self.alpha[0] + self.CY[1]*self.alpha[1])
-            t3 = np.ceil(self.CX[2]*self.alpha[0] + self.CY[2]*self.alpha[1])
+            sps = self.data[0].stats.sampling_rate
+            t1 = np.ceil((self.CX[0]*self.alpha[0] + self.CY[0]*self.alpha[1]) * sps) 
+            t2 = np.ceil((self.CX[1]*self.alpha[0] + self.CY[1]*self.alpha[1]) * sps)
+            t3 = np.ceil((self.CX[2]*self.alpha[0] + self.CY[2]*self.alpha[1]) * sps)
             tr.stats.station = "BFS"
             tr.data = np.roll(self.data[0].data, int(t1)) + np.roll(self.data[1].data, int(t2)) + np.roll(self.data[2].data, int(t3))
             ii = np.argmax(tr.data)
@@ -286,7 +309,7 @@ class Triads(object):
                 td.have_data = True
             else:
                 continue
-    def get_waveform(self, start=None, end=None, source=None, target_sps=1):
+    def get_waveform(self, start=None, end=None, source=None, cfg=None):
         """
         1. Request data from source.
         2. Preprocess: filter, resample and align.
@@ -294,17 +317,19 @@ class Triads(object):
         import concurrent.futures
         from tqdm import tqdm
 
+        if cfg == None:
+            raise ValueError("No configuration")
+        fl = cfg['Filter'][0]
+        fh = cfg['Filter'][1]
+
         if not start:
             end = UTCDateTime.now() -10
             start = end - 300
-        if not source:
-            raise ValueError("No data source defined")
-        if source[0] == "sds":
-            sds_arch = source[1]
         processes = []
         with concurrent.futures.ProcessPoolExecutor() as executor:
             for i, td in enumerate(self.triads):
-                p = executor.submit(request_data,i, td, start, end, target_sps,sds_arch)
+               
+                p = executor.submit(request_data,i, td, start, end, cfg['target_sps'],fl,fh, cfg['Source'])
                 processes.append(p)
             pbar = tqdm(total=len(processes), desc='Overall progress')
             for f in concurrent.futures.as_completed(processes):
@@ -328,6 +353,7 @@ class Triads(object):
                 sta.append([td.clon,td.clat,td.azm, 1.0])
         s = pd.DataFrame(sta)
         fig.plot(x=center[0],y=center[1],style="a0.8c",color="red")
-        fig.plot(x=s[0],y=s[1], style="V0.2c+ea+bc",direction=[s[2],s[3]],pen="0.5p",color="cyan" )
+        if len(s) > 1:
+            fig.plot(x=s[0],y=s[1], style="V0.2c+ea+bc",direction=[s[2],s[3]],pen="0.5p",color="cyan" )
         fig.savefig('triad.jpg')
         fig.show()
