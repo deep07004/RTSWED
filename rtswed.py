0f4fae121491968da073e5dee2c4e4c86850b88d
dipankar Saikia <deepngri@gmail.com>
2023-03-10 18:27:39 +0530

    need clustering

--- rtswed.py
+++ rtswed.py
@@ -4,7 +4,7 @@ from obspy.core import UTCDateTime
 from obspy import read_inventory, Inventory
 from obspy import read_inventory
 import pandas as pd
-import pygmt
+import numpy as np
 import maketriad
 
 # To read yaml configuration file to stream
@@ -22,6 +22,16 @@ args = parser.parse_args()
 # to proceed further 
 cfg = read(args.config)
 inv = read_inventory(os.path.join(cfg['Work_dir'],cfg['Inventory']))
+# Cleanup the inventory and select only three channels per station
+_tmp_HH = inv.select(channel='HH?')
+_tmp_BH = inv.select(channel='BH?')
+_tmp_BH_stations = [ sta.code for nt in _tmp_BH for sta in nt]
+_tmp_HH_stations = [ sta.code for nt in _tmp_HH for sta in nt]
+for a in _tmp_BH_stations:
+    if a not in _tmp_HH_stations:
+        _tmp_HH.extend(_tmp_BH.select(station=a))
+inv = _tmp_HH
+
 #inv =  read_inventory('../Turkey.xml')
 triads = maketriad.maketriad(inv, minlen=cfg['Triad']['min_side_length'], \
     maxlen=cfg['Triad']['max_side_length'], minang=cfg['Triad']['min_angle'],\
@@ -29,18 +39,20 @@ triads = maketriad.maketriad(inv, minlen=cfg['Triad']['min_side_length'], \
 
 #ot = UTCDateTime("2022-1-15T04:14:45")
 ot = UTCDateTime("2023-02-06T01:17:35") 
-fp = open("out.txt","w")
-for i in range(13):
+fp = open("Tout.txt","w")
+for i in range(20):
     t1= ot + 180*i
     t2 = t1 + 600
     triads.get_waveform(start=t1,end=t2,cfg=cfg)
     for td in triads:
         td.correlate(shift=3000)
         td.beamform()
+    triads.polarization(method=cfg['Source'])
     a = triads.select_active()
     for i, td in enumerate(triads):
         if td.detection:
-            s = "%s,%0.4f,%0.4f,%0.2f,%0.2f,%s,%0.2f\n" %(t1,td.clat, td.clon, td.azm, td.apvel, td.beam[0],td.beam[1])
+            s = "%s,%0.4f,%0.4f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%s,%0.2f\n" %(t1,td.clat, \
+               td.clon, np.mean(td.cc),np.mean(td.dt), td.azm, td.polazm, td.apvel, td.beam[0],td.beam[1])
             print(s)
             fp.write(s)
 fp.close()
